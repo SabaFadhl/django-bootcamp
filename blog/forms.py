2157c9343de04ff4b115cce657d4e83e38572ac3
imorghim <imorghim@open-alt.com>
2022-01-27 11:56:28 +0300

    work: 27-1

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,6 @@ from django import forms
 class PostForm(forms.Form):
     title = forms.CharField(label='Title', max_length=100)
     text = forms.IntegerField(label='Mobile Number')
+
+
+    # def save()
